4d8c2710e9449ea743c8dd5763bbbe6617bcc3bd
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-10-17 12:51:48 +0100

    Add basic charity model

--- Linking_Charities/testapp/models.py
+++ Linking_Charities/testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Charity(models.Model):
+    name = models.CharField(max_length=200)
+    registered_id = models.IntegerField(default=0)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
