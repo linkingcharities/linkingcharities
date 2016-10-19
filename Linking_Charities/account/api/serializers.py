ee71154e9649416f9e88d3212467006306cf6592
Mingfei Yao <ming@mingfeis-mbp.home>
2016-10-19 21:59:08 +0100

    consistent use of tabs

--- Linking_Charities/account/api/serializers.py
+++ Linking_Charities/account/api/serializers.py
@@ -18,16 +18,16 @@ class UserCreateSerializer(ModelSerializer):
             'password',
             'email',
         ]
- 	extra_kwargs = {"password":{"write_only": True}}
+        extra_kwargs = {"password" : {"write_only" : True} }
     
     def create(self, validated_data):
         username = validated_data['username']
         email = validated_data['email']
         password = validated_data['password']
         user_obj = User(
-	    username = username,
-	    email = email
+            username = username,
+            email = email
         )
-	user_obj.set_password(password)
-	user_obj.save()
+        user_obj.set_password(password)
+        user_obj.save()
         return validated_data
