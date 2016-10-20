e75ee2093a970eb3f440d7e3bd98e0c88fd31d7f
Mingfei Yao <ming@mingfeis-mbp.home>
2016-10-20 22:43:34 +0100

    login code

--- Linking_Charities/account/api/serializers.py
+++ Linking_Charities/account/api/serializers.py
@@ -1,16 +1,20 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
+from django.db.models import Q
+
 from rest_framework.serializers import (
     HyperlinkedIdentityField,
     ModelSerializer,
     SerializerMethodField,
-    ValidationError
+    ValidationError,
+    CharField,
+    EmailField,
 )
 
 User = get_user_model()
 
-class UserCreateSerializer(ModelSerializer):
+class AccountCreateSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = [
@@ -31,3 +35,39 @@ class UserCreateSerializer(ModelSerializer):
         user_obj.set_password(password)
         user_obj.save()
         return validated_data
+
+class AccountLoginSerializer(ModelSerializer):
+    token = CharField(allow_blank=True, read_only=True)
+    username = CharField(required=True)
+    email = EmailField(label='Email Address')
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'token',
+        ]
+        extra_kwargs = {"password": {"write_only": True} }
+    def validate(self, data):
+        user_obj = None
+        username = data.get("username", None)
+        password = data.get("password")
+        if not username:
+            raise ValidationError("Username is required.")
+        user = User.objects.filter(
+            Q(username=username)
+        ).distinct()
+        user = user.exclude(email__isnull=True).exclude(email__iexact='')
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("This username is not valid.")
+        
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect password.")
+
+        data["token"] = "token" 
+
+        return data
--- Linking_Charities/account/api/urls.py
+++ Linking_Charities/account/api/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 from .views import (
     AccountCreateAPIView,
+    AccountLoginAPIView,
 )
 
 urlpatterns = [
     url(r'register/', AccountCreateAPIView.as_view(), name='register'),
+    url(r'login/', AccountLoginAPIView.as_view(), name='login',)
 ]
