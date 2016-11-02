fe443548fabc4731822f5fa1529c26c208a63cd6
Junqiang Qian <junqiang.qian@hotmail.co.uk>
2016-11-02 14:12:28 +0000

    Filtering across name and type

--- Linking_Charities/charity/views.py
+++ Linking_Charities/charity/views.py
@@ -3,8 +3,11 @@ from rest_framework import generics
 from charity.models import Charity
 from charity.serializers import CharitySerializer
 from rest_framework.permissions import AllowAny
+import django_filters
 
 class ListCreateCharities(generics.ListCreateAPIView):
     permission_classes = [AllowAny]
     queryset = Charity.objects.all()
     serializer_class = CharitySerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_fields = ('type','name',)
