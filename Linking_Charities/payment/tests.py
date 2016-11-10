7dcee251b8a7d48e3bc04122c05c81fee95e0cf6
Mingfei Yao <ming@mingfeis-mbp.home>
2016-11-10 21:55:56 +0000

    added tests for account and payments

--- Linking_Charities/payment/tests.py
+++ Linking_Charities/payment/tests.py
@@ -1,17 +1,19 @@
 from django.test import TestCase
 from payment.models import *
-from django.utils import timezone
+from django.utils import timezone, dateparse
+import json
 
 # Create your tests here.
 
 class PaymentTestCase(TestCase):
 
     def setUp(self):
+        self.time = timezone.now()
         Payment.objects.create(username='ming'
                          , charity='charilink@gmail.com'
                          , currency='USD'
                          , amount='1'
-                         , date=timezone.now())
+                         , date=self.time)
         return
 
     def testGetPaymentAPIWithUser(self):
@@ -19,5 +21,30 @@ class PaymentTestCase(TestCase):
         response = self.client.get('/api/show_payment'
                                   , { 'username' : 'ming' }
                                   , format='json')
+        
+        returnData = json.loads(response.content.decode())
+        self.assertEquals(len(returnData),1)
+        entry = returnData[0]
+        self.assertEquals(entry['username'], 'ming')
+        self.assertEquals(entry['charity'], 'charilink@gmail.com')
+        self.assertEquals(entry['currency'], 'USD')
+        self.assertEquals(entry['amount'], 1)
+        self.assertEquals(dateparse.parse_datetime(entry['date']), self.time)
+        
+        print("Get payment API passed.")
 
-        print(response.content)
+    def testGetPaymentAPICanGetMultiplePayments(self):
+        
+        Payment.objects.create(username='ming'
+                         , charity='someothercharity@gmail.com'
+                         , currency='USD'
+                         , amount='23'
+                         , date=timezone.now())
+        
+        response = self.client.get('/api/show_payment'
+                                   , { 'username': 'ming' }
+                                   , format='json')
+        returnData = json.loads(response.content.decode())
+        self.assertEquals(len(returnData),2)
+
+        print("Get payment API multiple payments passed.")
