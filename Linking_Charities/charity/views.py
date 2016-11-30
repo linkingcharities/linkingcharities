37068aafa560d3cb269c86e39902af6284412859
Mingfei Yao <ming@mingfeis-mbp.home>
2016-11-30 13:42:55 +0000

    fixing total income dat

--- Linking_Charities/charity/views.py
+++ Linking_Charities/charity/views.py
@@ -40,6 +40,7 @@ class ListCreateCharities(generics.ListCreateAPIView):
             user = User.objects.filter(
                 Q(username=username)
             )
+            print('reached')
             if not user.exists():
                 raise ValidationError("No account provided.")
             charity_account = CharityAccount.objects.filter(
