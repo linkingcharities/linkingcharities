6ad0d48996222dcc07d90ca4c33062d99d61aed1
Mingfei Yao <my1914@ic.ac.uk>
2016-12-30 19:38:44 +0000

    adding https server

--- Linking_Charities/Linking_Charities/settings.py
+++ Linking_Charities/Linking_Charities/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'sslserver',
     'rest_framework',
     'corsheaders',
     'rest_framework.authtoken',
